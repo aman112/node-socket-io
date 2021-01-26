cb6760ff0ed3cf8ab26f65ebf30bc65eeba9015c
Aman Aggarwal <aman.aggarwal195@gmail.com>
2021-01-26 21:39:22 +0530

    Server serving Index.html file

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express=require('express');
-const app=express();
-
+var path = require('path');
 const propertiesReader=require('properties-reader');
 const configProperties=propertiesReader('config.properties');
 const hostname=configProperties.get('hostname');
 const port=configProperties.get('port');
 
+const app=express();
+
+app.use(express.static(path.join(__dirname, 'client')));
+
 app.get('/',function(req,res){
     res.statusCode=200;
-    res.contentType('text/plain');
-    res.end('Request came at Express Server');
+    res.contentType('text/html');
+    res.sendFile('index.html');
 });
 
 app.listen(port,hostname,()=>{
